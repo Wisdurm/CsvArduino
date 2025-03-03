a9e7508262d08c15037b5a5ccdebffecc6d110ab
Wisdurm <luukas.kola@gmail.com>
2025-03-03 17:33:00 +0200

    Now splits simultaneous notes into seperate files, while still creating one where they're all combined.

--- CvsArduino/Program.cs
+++ CvsArduino/Program.cs
@@ -2,6 +2,12 @@
 
 namespace CvsArduino
 {
+    struct NoteSequence ()
+    {
+        public List<int> notes = [];
+        public List<int> noteStarts = [];
+        public List<int> noteLengths = [];
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -11,11 +17,10 @@ namespace CvsArduino
                 string[] file = File.ReadAllLines(args[0]);
 
                 //Parse
-                List<int> notesl = [];
-                List<int> noteStartsl = [];
-                List<int> noteLengthsl = [];
+                List<NoteSequence> noteSequences = [];
+                NoteSequence combined = new();
 
-                Dictionary<string, int> ongoingNotes = [];
+                Dictionary<string, int[]> ongoingNotes = [];
 
                 for (int i = 0; i < file.Length; i++)
                 {
@@ -28,51 +33,65 @@ namespace CvsArduino
                     {
                         if (parse[2] == " Note_on_c" && parse[5] != " 0") //Start note
                         {
-                            ongoingNotes.Add(parse[4], Convert.ToInt32(parse[1])); //Keep track of started notes (key = note, value = start)
+                            ongoingNotes.Add(parse[4], [ Convert.ToInt32(parse[1]), ongoingNotes.Count]); //Keep track of started notes (key = note, value = startTime and "channel")
                         }
                         else //Stop note
                         {
                             int note = Convert.ToInt32(parse[4]);
-                            ongoingNotes.Remove(parse[4], out int noteStart); //When a note stops, get it's start info from the dictionary
+                            ongoingNotes.Remove(parse[4], out int[] value); //When a note stops, get it's start info from the dictionary
+                            int noteStart = value[0];
+                            int channel = value[1];
 
-                            notesl.Add(note);
-                            noteStartsl.Add(noteStart);
-                            noteLengthsl.Add(Convert.ToInt32(parse[1]) - noteStart); //End - start
+                            if (noteSequences.Count - 1 < channel)
+                                noteSequences.Add(new NoteSequence());
+
+                            noteSequences[channel].notes.Add(note);
+                            noteSequences[channel].noteStarts.Add(noteStart);
+                            noteSequences[channel].noteLengths.Add(Convert.ToInt32(parse[1]) - noteStart); //End - start
+
+                            combined.notes.Add(note);
+                            combined.noteStarts.Add(noteStart);
+                            combined.noteLengths.Add(Convert.ToInt32(parse[1]) - noteStart);
                         }
                     }
                 }
 
-                int[] notes = [.. notesl];
-                int[] noteStarts = [.. noteStartsl];
-                int[] noteLengths = [.. noteLengthsl];
-
-                File.WriteAllText("output/notes.txt", string.Join(",", notes));
-                File.WriteAllText("output/notesS.txt", string.Join(",", noteStarts));
-                File.WriteAllText("output/notesL.txt", string.Join(",", noteLengths));
+                for (int i = 0; i < noteSequences.Count; i++)
+                {
+                    File.WriteAllText($"output/notes{i}.txt", string.Join(",", noteSequences[i].notes));
+                    File.WriteAllText($"output/notes{i}S.txt", string.Join(",", noteSequences[i].noteStarts));
+                    File.WriteAllText($"output/notes{i}L.txt", string.Join(",", noteSequences[i].noteLengths));
+                }
+                //Combined
+                File.WriteAllText($"output/notesC.txt", string.Join(",", combined.notes));
+                File.WriteAllText($"output/notesCS.txt", string.Join(",", combined.noteStarts));
+                File.WriteAllText($"output/notesCL.txt", string.Join(",", combined.noteLengths));
 
 
                 //Play track (debugging)
-                const float noteMultiplier = 1f;
-                const int delayMultiplier = 1;
+                //const float noteMultiplier = 1f;
+                //const int delayMultiplier = 1;
 
-                int time = 0;
-                int j = 0;
-                while (j < notes.Length)
-                {
-                    if (noteStarts[j] == time)
-                    {
-                        Console.Beep(MidiToFrequency(notes[j]), (int)(noteLengths[j] * noteMultiplier)); //boohoo intellicode, only works on windows waaaa
-                        j++;
-                        if (noteStarts[j + 1] == time) //Don't crash on simultaneous notes //Ported code from js, this can and will cause an IndexOutOfRangeException, but too lazy to fix
-                            time += noteLengths[j];
-                    }
-                    else
-                    {
-                        time++;
-                        if (time % delayMultiplier == 0)
-                            Thread.Sleep(1);
-                    }
-                }
+                //Not only is it impossible to play multiple notes at once using console.beep(), but I was also too lazy to make this 
+
+                //int time = 0;
+                //int j = 0;
+                //while (j < notes.Length)
+                //{
+                //    if (noteStarts[j] == time)
+                //    {
+                //        Console.Beep(MidiToFrequency(notes[j]), (int)(noteLengths[j] * noteMultiplier)); //boohoo intellicode, only works on windows waaaa
+                //        j++;
+                //        if (noteStarts[j + 1] == time) //Don't crash on simultaneous notes //Ported code from js, this can and will cause an IndexOutOfRangeException, but too lazy to fix
+                //            time += noteLengths[j];
+                //    }
+                //    else
+                //    {
+                //        time++;
+                //        if (time % delayMultiplier == 0)
+                //            Thread.Sleep(1);
+                //    }
+                //}
 
             }
             else //No file
